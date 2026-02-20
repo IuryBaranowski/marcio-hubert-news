979461a5bf347deb6155d32c530e35c6b7a692c5
Iury Barros <iury@pinnaclemfg.net>
2026-02-20 11:34:55 -0500

    feat: adjust

--- app/components/entrevistas/entrevistasImages.ts
+++ app/components/entrevistas/entrevistasImages.ts
@@ -3,6 +3,6 @@
  * Arquivos: entrevista (1).jpeg até entrevista (15).jpeg
  */
 export const ENTREVISTAS_IMAGES: string[] = Array.from(
-  { length: 19 },
+  { length: 62 },
   (_, i) => `/images/Entrevistas/entrevista (${i + 1}).jpeg`
 );
